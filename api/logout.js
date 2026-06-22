40da29106961820148710889b5eded26fbc0ceac
francorodo2001-byte <francorodo2001@gmail.com>
2026-06-22 09:47:12 -0300

    Add files via upload

---
+++ api/logout.js
@@ -0,0 +1,8 @@
+// /api/logout.js
+export default async function handler(req, res) {
+  res.setHeader(
+    'Set-Cookie',
+    'atalaya_auth=; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=0'
+  );
+  return res.status(200).json({ ok: true });
+}
